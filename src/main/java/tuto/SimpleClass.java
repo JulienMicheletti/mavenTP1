ba2560da2d51733589a2603d80fd30f676cb19d3
Julien Micheletti <j.micheletti@orange.fr>
2019-09-20 13:46:41 +0100

    Test unitaire avec assertion fausse

---
+++ src/main/java/tuto/SimpleClass.java
@@ -0,0 +1,7 @@
+package tuto;
+
+public class SimpleClass {
+    public int addition(int nb1, int nb2){
+        return nb1 + nb2;
+    }
+}
---
+++ src/test/java/tuto/SimpleClassTest.java
@@ -0,0 +1,15 @@
+package tuto;
+
+import org.junit.Test;
+import tuto.SimpleClass;
+
+import static org.junit.Assert.*;
+
+public class SimpleClassTest {
+
+    @Test
+    public void addition() {
+        SimpleClass sc = new SimpleClass();
+        assertEquals(5, sc.addition(2, 2));
+    }
+}
